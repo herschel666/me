9040b83764b08a85077808df45189ec9eb7964c5
Emanuel Kluge <mail@emanuel-kluge.de>
2019-12-07 10:47:12 +0100

    Initial commit

---
+++ .posthtmlrc.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const generateScopedName = isProd
+  ? '[hash:base64:5]'
+  : '[name]__[local]___[hash:base64:5]';
+const normalizeCSSFile = isProd
+  ? 'https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/0.5.0/modern-normalize.min.css'
+  : `../${path.relative(__dirname, require.resolve('modern-normalize'))}`;
+
+module.exports = {
+  plugins: {
+    'posthtml-expressions': {
+      locals: {
+        year: String(new Date().getFullYear()),
+        normalizeCSSFile,
+      },
+    },
+    'posthtml-postcss-modules': {
+      root: './src',
+      plugins: [require('autoprefixer')],
+      generateScopedName,
+    },
+  },
+};
